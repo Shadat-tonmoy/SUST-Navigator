7985df1a1217e7477e9147bd5b9d89dfbc4df427
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2018-11-05 12:23:59 +0600

    all log removed

--- app/src/main/java/shadattonmoy/sustnavigator/MainActivity.java
+++ app/src/main/java/shadattonmoy/sustnavigator/MainActivity.java
@@ -567,7 +567,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void openHelpFragment(View view) {
-        Log.e("Opening","Help");
+//        Log.e("Opening","Help");
         Intent intent = new Intent(MainActivity.this, HelpActivity.class);
         startActivity(intent);
 
--- app/src/main/java/shadattonmoy/sustnavigator/admin/view/ScanSyllabusFragment.java
+++ app/src/main/java/shadattonmoy/sustnavigator/admin/view/ScanSyllabusFragment.java
@@ -305,7 +305,7 @@ public class ScanSyllabusFragment extends android.app.Fragment {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            Log.e("Start","scanning");
+//            Log.e("Start","scanning");
             progressDialog = new ProgressDialog(activity);
             progressDialog.setTitle("Scanning Image");
             progressDialog.setMessage("Please Wait....");
@@ -330,13 +330,13 @@ public class ScanSyllabusFragment extends android.app.Fragment {
             foundText = new HashMap<>();
             firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
             firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
-            Log.e("Start","scanning processing");
+//            Log.e("Start","scanning processing");
             Task<FirebaseVisionText> result =
                     firebaseVisionTextDetector.detectInImage(firebaseVisionImage)
                             .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
                                 @Override
                                 public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                                    Log.e("Start","scanningDonne "+firebaseVisionText.getBlocks().size()+" is found");
+//                                    Log.e("Start","scanningDonne "+firebaseVisionText.getBlocks().size()+" is found");
                                     for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks()) {
                                         Rect boundingBox = block.getBoundingBox();
                                         Point[] cornerPoints = block.getCornerPoints();
@@ -350,7 +350,7 @@ public class ScanSyllabusFragment extends android.app.Fragment {
                                                 if (foundText.get(textBlockValue) == null || !foundText.get(textBlockValue)) {
                                                     detectedTexts.add(textBlockValue);
                                                     foundText.put(textBlockValue, true);
-                                                    Log.e("OutputText", textBlockValue);
+//                                                    Log.e("OutputText", textBlockValue);
                                                 }
 //                                                detectedTexts.add(line.getText());
                                             }
@@ -375,7 +375,7 @@ public class ScanSyllabusFragment extends android.app.Fragment {
 
             DetectedTextDialog detectedTextDialog = new DetectedTextDialog(context, detectedTexts,session,semester,dept,mCurrentPhotoPath);
             detectedTextDialog.show(getActivity().getFragmentManager(), "detectedTextDialog");
-            Log.e("dialog","showing");
+//            Log.e("dialog","showing");
 
         }
     }
